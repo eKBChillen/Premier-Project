0e441e879a5197b6f081c1d2f0d58d65515a0e74
eKBChillen <bfergy06@gmail.com>
2026-02-01 17:49:07 -0600

    Conatct page update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function sendMail() {
     let params = {
         name: document.getElementById("name").value,
+        number: document.getElementById("number").value,
         email: document.getElementById("email").value,
         message: document.getElementById("message").value,
     }
-    email.js.send("service_00kd56m", "template_dgw822x", params).then(alert("Message sent successfully"))
+    emailjs.send("service_00kd56m", "template_r5cs5jw", params).then(alert("Message sent successfully"))
 }
 import { Carousel, initMDB } from "mdb-ui-kit";
 
