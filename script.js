f797725385609d7687c97cabdf889b69f451353f
eKBChillen <bfergy06@gmail.com>
2026-02-01 18:08:02 -0600

    Javascript update

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function sendMail() {
-    let params = {
-        name: document.getElementById("name").value,
-        number: document.getElementById("number").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+    const templateParams = {
+        name: document.querySelector("#name").value,
+        number: document.querySelector("#number").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
     }
-    emailjs.send("service_00kd56m", "template_r5cs5jw", params).then(alert("Message sent successfully"))
-}
-import { Carousel, initMDB } from "mdb-ui-kit";
-
-initMDB({ Carousel });
+    emailjs
+    .send("service_00kd56m", "template_r5cs5jw", templateParams)
+    .then(() => {
+        alert("Message sent successfully");
+    })
+    .catch((error) => {
+        console.log("Error sending message:", error);
+        alert("There was an error sending your message. Please try again later.");
+    });
+}   
